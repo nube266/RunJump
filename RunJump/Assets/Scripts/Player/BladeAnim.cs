bc46e25bcbded503c3edd68d1ccccf859f84ea76
atmarktrpg4@gmail.com <atmarktrpg4@gmail.com>
2019-07-25 20:37:58 +0900

    斬撃アニメーションを新規作成

---
+++ RunJump/Assets/Scripts/Player/BladeAnim.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeAnim : MonoBehaviour
+{
+    void OnAnimationFinish() {
+        Destroy(gameObject);
+    }
+}
