12b88e750666cc6cd82ff2963308f1030333e174
YusukeMiake <miake@aisl.cs.tut.ac.jp>
2019-08-14 10:16:18 +0900

    ステージセレクトの追加

--- RunJump/Assets/Scripts/Other/TitleManager.cs
+++ RunJump/Assets/Scripts/Other/TitleManager.cs
@@ -4,9 +4,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
-    void Update () {
+    private GameObject[] stageText = new GameObject[3];
+    private int selectStageNum;
+
+    private void Start () {
+        selectStageNum = 1;
+        for (int i = 0; i < stageText.Length; i++) {
+            stageText[i] = GameObject.Find ("Stage" + (i + 1).ToString () + "Text");
+        }
+    }
+    private void Update () {
+        // input
         if (Input.GetKeyDown (KeyCode.S)) {
-            SceneManager.LoadScene ("stage1"); //選択画面に移動
+            SceneManager.LoadScene ("stage" + selectStageNum.ToString ());
+        }
+        if (Input.GetKeyDown (KeyCode.DownArrow) && selectStageNum != stageText.Length) {
+            selectStageNum++;
+        }
+        if (Input.GetKeyDown (KeyCode.UpArrow) && selectStageNum != 1) {
+            selectStageNum--;
+        }
+        // color update
+        this.UpdateColor ();
+    }
+
+    private void UpdateColor () {
+        for (int i = 0; i < stageText.Length; i++) {
+            if (selectStageNum == i + 1) {
+                stageText[i].gameObject.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, 1f);
+            } else {
+                stageText[i].gameObject.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, 0.5f);;
+            }
         }
     }
 }
