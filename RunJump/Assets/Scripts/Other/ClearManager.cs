4d9fbdd9da7a93919ad9bd316e733ce223cffdad
YusukeMiake <miake@aisl.cs.tut.ac.jp>
2019-08-22 16:14:06 +0900

    クリア時とゲームオーバー時のテキストを修正

--- RunJump/Assets/Scripts/Other/ClearManager.cs
+++ RunJump/Assets/Scripts/Other/ClearManager.cs
@@ -8,14 +8,14 @@ public class ClearManager : MonoBehaviour {
     [SerializeField] private string nextStageName = "title";
     private bool BossDiedFlag = false; // ボスが死んでいるならばtrue
     private GameObject cameraObject = null; // メインカメラのオブジェクト
-    
+
     [Header ("決定音")]
     [SerializeField] private AudioClip decisionAudio;
     AudioSource audioSource;
     // Start is called before the first frame update
     void Start () {
         cameraObject = GameObject.FindGameObjectWithTag ("MainCamera");
-        audioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
@@ -23,7 +23,7 @@ public class ClearManager : MonoBehaviour {
         if (BossDiedFlag == true) {
             if (Input.GetKeyDown (KeyCode.S)) {
                 SceneManager.LoadScene (nextStageName);
-                audioSource.PlayOneShot(decisionAudio);
+                audioSource.PlayOneShot (decisionAudio);
             }
         }
     }
@@ -38,6 +38,9 @@ public class ClearManager : MonoBehaviour {
     //---------------------Game Clear時のテキスト表示(先頭)---------------------//
     [Header ("GAME CLEAR時に表示されるテキスト")]
     [SerializeField] private GameObject GameClearTextObject = null;
+    [SerializeField] private GameObject PressTextObject = null;
+    [SerializeField] private GameObject STextObject = null;
+    [SerializeField] private GameObject KeyTextObject = null;
     [Header ("GAME CLEAR時に表示されるテキストの位置に関するオフセット(X方向)")]
     [SerializeField] private float GameClearTextOffsetX = 0.5f;
     [Header ("GAME Clear時に表示されるテキストの位置に関するオフセット(Y方向)")]
@@ -50,6 +53,33 @@ public class ClearManager : MonoBehaviour {
                     new Vector3 (
                         cameraObject.transform.position.x + GameClearTextOffsetX,
                         cameraObject.transform.position.y + GameClearTextOffsetY,
+                        this.transform.position.z
+                    ), // 位置
+                    Quaternion.identity
+                ); // 角度
+                Instantiate (
+                    PressTextObject, // 生成するPrefab
+                    new Vector3 (
+                        cameraObject.transform.position.x + GameClearTextOffsetX - 2.0f,
+                        cameraObject.transform.position.y + GameClearTextOffsetY - 2.0f,
+                        this.transform.position.z
+                    ), // 位置
+                    Quaternion.identity
+                ); // 角度
+                Instantiate (
+                    STextObject, // 生成するPrefab
+                    new Vector3 (
+                        cameraObject.transform.position.x + GameClearTextOffsetX + 0.5f,
+                        cameraObject.transform.position.y + GameClearTextOffsetY - 2.0f,
+                        this.transform.position.z
+                    ), // 位置
+                    Quaternion.identity
+                ); // 角度
+                Instantiate (
+                    KeyTextObject, // 生成するPrefab
+                    new Vector3 (
+                        cameraObject.transform.position.x + GameClearTextOffsetX + 2.5f,
+                        cameraObject.transform.position.y + GameClearTextOffsetY - 2.0f,
                         this.transform.position.z
                     ), // 位置
                     Quaternion.identity
